28846b3ebf33a769867afec13ae3be08228c4615
dbal0503 <108775943+dbal0503@users.noreply.github.com>
2024-10-16 13:08:57 -0400

    Instructor getters and setters

--- source/Instructor.java
+++ source/Instructor.java
@@ -1,8 +1,14 @@
 public class Instructor extends Users {
+    public int phone_number;
+    public Specialization specialization;
+    public Availabilities[] availabilities;
     
-    public Instructor(String name, int id) {
+    public Instructor(String name, int id, int phone_number, Specialization specialization, Availabilities[] availabilities) {
         this.name = name;
         this.uniqueId = id;
+        this.phone_number = phone_number;
+        this.specialization = specialization;
+        this.availabilities = availabilities;
     }
 
     public String getName() {
@@ -19,5 +25,23 @@ public class Instructor extends Users {
 
     public void setId(int id) {
         this.uniqueId = id;
+    }
+    public int getPhoneNumber() {
+        return this.phone_number;
+    }
+    public int setPhoneNumber(int phone_number) {
+        this.phone_number = phone_number;
+    }
+    public Specialization getSpecialization() {
+        return this.specialization;
+    }
+    public void setSpecialization(Specialization specialization) {
+        this.specialization = specialization;
+    }
+    public Availabilities[] getAvailabilities() {
+        return this.availabilities;
+    }
+    public void setAvailabilities(Availabilities[] availabilities) {
+        this.availabilities = availabilities;
     }
 }
